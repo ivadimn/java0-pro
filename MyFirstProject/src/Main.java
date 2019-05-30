59abda64aeece569bc5a42a89168beb2057893a4
vadim <ivadimn@mail.ru>
2019-05-30 18:29:04 +0300

    new project

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Print test");
+        System.out.println("Arithmetic test - 24.0 * 0.1 = " + 24.0 * 0.1);
+    }
+}
