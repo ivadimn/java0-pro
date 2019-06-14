41b47dcb250aa9edf95eae03dedcd1315be77cb7
ivadimn <ivadimn@mail.ru>
2019-06-14 21:25:00 +0300

    NUMBERS

---
+++ Numbers/src/Loader.java
@@ -0,0 +1,10 @@
+public class Loader {
+    public static void main(String[] args) {
+        System.out.println("Byte MIN_VALUE: " + Byte.MIN_VALUE + ", MAX_VALUE: "  + Byte.MAX_VALUE);
+        System.out.println("Short MIN_VALUE: " + Short.MIN_VALUE + ", MAX_VALUE: " + Short.MAX_VALUE);
+        System.out.println("Int MIN_VALUE: " + Integer.MIN_VALUE + ", MAX_VALUE: " + Integer.MAX_VALUE);
+        System.out.println("Long MIN_VALUE: " + Long.MIN_VALUE + ", MAX_VALUE: " + Long.MAX_VALUE);
+        System.out.println("Float MIN_VALUE: " + Float.MIN_VALUE + ", MAX_VALUE: " + Float.MAX_VALUE);
+        System.out.println("Double MIN_VALUE: " + Double.MIN_VALUE + ", MAX_VALUE: " + Double.MAX_VALUE);
+    }
+}
