e4f8d7b797c9533648b95171f7094f76875a6f71
vadim <ivadimn@mail.ru>
2019-10-02 13:25:25 +0300

    new version Html

--- Html/src/Main.java
+++ Html/src/Main.java
@@ -5,8 +5,7 @@ import org.jsoup.select.Elements;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,20 +15,26 @@ public class Main {
         URL url = new URL(PATH);
         Document doc = Jsoup.parse(url, 10000);
         Elements elements = doc.select("img");
-        elements.forEach(e -> System.out.println(e.attr("src")));
-        String ss = "aaaa/123456.png";
-        System.out.println(ss.substring(ss.lastIndexOf("/") + 1));
+        elements.forEach(e -> getImage(e.attr("src")));
     }
 
     public static void getImage(String path) {
-        BufferedImage image;
         try {
             URL url = new URL(path);
-            image = ImageIO.read(url);
-
+            InputStream in = new BufferedInputStream(url.openStream());
+            FileOutputStream out = new FileOutputStream("images" + path.substring(path.lastIndexOf("/")));
+            byte[] buffer = new byte[4096];
+            int n = 0;
+            while((n = in.read(buffer, 0, 4096)) != -1) {
+                out.write(buffer, 0, n);
+            }
+            in.close();
+            out.flush();
+            out.close();
+            System.out.println("Сохранено - " + "images" + path.substring(path.lastIndexOf("/")));
         }
         catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("Не удалось получить изображение = " + ex.getMessage());
         }
     }
 }
