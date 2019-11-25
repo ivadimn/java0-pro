acece8370a439894e747dc0d09b3c5a3423a79f3
vadim <ivadimn@mail.ru>
2019-11-25 15:07:03 +0300

    new version Hibern1

--- Hibern1/src/main/java/Main.java
+++ Hibern1/src/main/java/Main.java
@@ -7,9 +7,14 @@ public class Main {
     public static void main(String[] args) {
 
         List<Student> studentList = new ArrayList<>();
+        List<Course> coursesList = new ArrayList<>();
         Session session = Util.getSessionFactory().openSession();
         studentList = (List<Student>)session.createQuery("from Student").list();
         studentList.forEach(System.out::println);
+        coursesList = (List<Course>) session.createQuery("from Course").list();
+        System.out.println("***************************************************");
+        coursesList.forEach(c -> System.out.println(c.getName()));
+
         session.close();
     }
 }
